333501021e635da7728a8a4dcac6c30168116dc7
viktor <vic308@mail.ru>
2021-12-27 15:55:25 +0300

    1-3-bzhoska: edit script

--- hotel-app/src/main/resources/db/changelog/02-changelog-master-insert-data.sql
+++ hotel-app/src/main/resources/db/changelog/02-changelog-master-insert-data.sql
@@ -1,17 +1,17 @@
 INSERT INTO hotel (avg_mark, convenience, location, name)
-VALUES (4.4, 'FiveStars', 'Egypt', 'Mercuri');
-INSERT INTO hotel (avg_mark, convenience, location, name)
-VALUES (4.2, 'FiveStars', 'Egypt', 'Aladdin');
-INSERT INTO hotel (avg_mark, convenience, location, name)
-VALUES (4.3, 'FiveStars', 'Egypt', 'Alibaba');
+VALUES (4.4, '5', 'Egypt', 'Mercuri');
 
-INSERT INTO common_user (email, first_name, passport, phone_number, second_name)
-VALUES ('vic308@mail.ru', 'viktor', 'ab123', '123456', 'bzhosko');
+INSERT INTO hotel_rating (count_of_marks, mark, hotel_id)
+VALUES (12, 4, 1);
 
-INSERT INTO room (accommodation, comfort, number_of_room, hotels_hotel_id, hotel_id)
-VALUES ('SINGLE', 'SUITE', 1, 1, 1);
-INSERT INTO room (accommodation, comfort, number_of_room, hotels_hotel_id, hotel_id)
-VALUES ('SINGLE', 'SUITE', 2, 1, 1);
+INSERT INTO room (accommodation, comfort, number_of_room, hotels_id)
+VALUES ('TRIPLE', 'DELUXE', 1, 1);
 
-INSERT INTO booking (booking_status, date_chek_in, date_chek_out, hotel_hotel_id, room_room_id, users_user_id)
-VALUES (0, '2021-01-01', '2021-01-05', 1, 1, 1);
+INSERT INTO booking (booking_status, date_chek_in, date_chek_out, hotel_id, room_id)
+VALUES (0, '2021-01-01', '2021-01-10', 1, 1);
+
+INSERT INTO common_user (email, first_name, passport, phone_number, second_name,  booking_id)
+VALUES ('vic308@mail.ru', 'viktor', 'ab123', '123456', 'bzhosko', 1);
+
+INSERT INTO hotel_favorites (hotel_id, user_id)
+VALUES (1, 1);
